e894fa6c92b2d3a2da6eebe8d6dcc97cd2e1db25
sobir0990 <sobir_0990@mail.ru>
2021-08-04 11:40:21 +0500

    lang

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -2,13 +2,15 @@ import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = 'ffb96f83cdd0cbde1ece5527c979d76e';
+const LANG = 'ru';
 
 export const fetchWeather = async (query) => {
     const {data} = await axios.get(URL, {
         params: {
             q: query,
             units: 'metric',
-            appid: API_KEY
+            appid: API_KEY,
+            lang: LANG
         }
     });
     return data;
